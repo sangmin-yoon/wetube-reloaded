994836062b909fcc1d1607e60838cc6a3f635f88
sangmin-yoon <85691654+sangmin-yoon@users.noreply.github.com>
2021-07-19 19:21:05 +0900

    .

---
+++ src/server.js
@@ -0,0 +1,21 @@
+import express from "express";
+
+const PORT = 4000;
+
+const app = express();
+
+const handleHome = (req, res) => {
+    return res.send("<h1>I still love you</h1>");
+};
+
+const handleLogin = (req, res) => {
+    return res.send("Login here.");
+}
+
+app.get("/", handleHome);
+app.get("/login", handleLogin);
+
+const handleListening = () =>
+    console.log(`✅ Server listening on port http://localhost:${PORT} 🚀`);
+
+app.listen(PORT, handleListening);
