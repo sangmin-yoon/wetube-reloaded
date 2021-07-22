96803d81bfa7ccbb1310ad31fe8dc78414b91961
sangmin-yoon <85691654+sangmin-yoon@users.noreply.github.com>
2021-07-22 18:14:22 +0900

    Routers

---
+++ readme.md
@@ -0,0 +1,16 @@
+# Wetube Reloaded
+
+/ -> Home
+/join -> join
+/login -> Login
+/search -> Search
+
+/users/:id -> See User
+/users/logout -> Log Out
+/users/edit -> Edit MY Profile
+/users/delete -> Delete MY Profile
+
+/videos/:id -> See Videos
+/videos/:id/edit -> Edit Video
+/videos/:id/delete -> Delete Video
+/videos/upload -> Upload Video
--- src/server.js
+++ src/server.js
@@ -1,21 +1,20 @@
 import express from "express";
+import morgan from "morgan";
+import globalRouter from "./routers/globalRouter";
+import userRouter from "./routers/userRouter";
+import videoRouter from "./routers/videoRouter";
 
 const PORT = 4000;
 
 const app = express();
+const logger = morgan("dev");
+app.use(logger);
 
-const handleHome = (req, res) => {
-    return res.send("<h1>I still love you</h1>");
-};
-
-const handleLogin = (req, res) => {
-    return res.send("Login here.");
-}
-
-app.get("/", handleHome);
-app.get("/login", handleLogin);
+app.use("/", globalRouter);
+app.use("/videos", videoRouter);
+app.use("/users", userRouter);
 
 const handleListening = () =>
-    console.log(`✅ Server listening on port http://localhost:${PORT} 🚀`);
+  console.log(`✅ Server listening on port http://localhost:${PORT} 🚀`);
 
 app.listen(PORT, handleListening);
