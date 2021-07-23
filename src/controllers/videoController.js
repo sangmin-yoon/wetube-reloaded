1c2103e328effaf1672779035701a27db0d3ecef
sangmin-yoon <85691654+sangmin-yoon@users.noreply.github.com>
2021-07-24 01:18:40 +0900

    pug

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,14 +1,39 @@
-export const trending = (req, res) => res.send("Home Page Videos");
-export const see = (req, res) => {
-  console.log(req.params);
-  return res.send(`Watch Video #${req.params.id}`);
+export const trending = (req, res) => {
+  const videos = [
+    {
+      title: "First Video",
+      rating: 5,
+      comments: 2,
+      createAt: "2 minutes ago",
+      views: 59,
+      id: 1,
+    },
+    {
+      title: "Second Vided",
+      rating: 5,
+      comments: 2,
+      createAt: "2 minutes ago",
+      views: 59,
+      id: 1,
+    },
+    {
+      title: "Third Video",
+      rating: 5,
+      comments: 2,
+      createAt: "2 minutes ago",
+      views: 59,
+      id: 1,
+    },
+  ];
+  return res.render("home", { pageTitle: "Home", videos });
 };
-export const edit = (req, res) => {
-  console.log(req.params);
-  res.send("Edit");
-};
+export const see = (req, res) => res.render("watch", { pageTitle: "watch" });
+export const edit = (req, res) => res.render("edit", { pageTitle: "edit" });
+
 export const search = (req, res) => res.send("Search");
+
 export const upload = (req, res) => res.send("Upload");
+
 export const deleteVideo = (req, res) => {
   console.log(req.params);
   res.send("Delete Video");
