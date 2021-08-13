7f5dc38b3b51b15b65a3a4f4f9cad8464dbb119e
sangmin-yoon <85691654+sangmin-yoon@users.noreply.github.com>
2021-08-13 15:07:42 +0900

    playerVolume,mute,pause

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -13,11 +13,10 @@ export const home = async (req, res) => {
 export const watch = async (req, res) => {
   const { id } = req.params;
   const video = await Video.findById(id).populate("owner");
-  console.log(video);
-  if (video) {
-    return res.render("watch", { pageTitle: video.title, video });
+  if (!video) {
+    return res.render("404", { pageTitle: "Video not found." });
   }
-  return res.render("404", { pageTitle: "Video not found." });
+  return res.render("watch", { pageTitle: video.title, video });
 };
 
 export const getEdit = async (req, res) => {
