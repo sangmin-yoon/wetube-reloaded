ab3f9b72996b19fa35a85519f37ee20f93115e58
sangmin-yoon <85691654+sangmin-yoon@users.noreply.github.com>
2021-08-21 05:17:20 +0900

    s3

--- src/server.js
+++ src/server.js
@@ -27,12 +27,12 @@ app.use(
   })
 );
 
-app.use((req, res, next) => {
-  res.header("Cross-Origin-Embedder-Policy", "require-corp");
-  res.header("Cross-Origin-Opener-Policy", "same-origin");
-  res.header("Access-Control-Allow-Origin", "*");
-  next();
-});
+// app.use((req, res, next) => {
+//   res.header("Cross-Origin-Embedder-Policy", "require-corp");
+//   res.header("Cross-Origin-Opener-Policy", "same-origin");
+
+//   next();
+// });
 
 app.use(flash());
 app.use(localMiddleware);
