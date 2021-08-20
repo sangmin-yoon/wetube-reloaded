271e63b054e8ae6fa3e22bc29977ffffbdeb6e9b
sangmin-yoon <85691654+sangmin-yoon@users.noreply.github.com>
2021-08-21 04:14:28 +0900

    cors

--- src/server.js
+++ src/server.js
@@ -30,6 +30,7 @@ app.use(
 app.use((req, res, next) => {
   res.header("Cross-Origin-Embedder-Policy", "require-corp");
   res.header("Cross-Origin-Opener-Policy", "same-origin");
+  res.header("Access-Control-Allow-Origin", "*");
   next();
 });
 
